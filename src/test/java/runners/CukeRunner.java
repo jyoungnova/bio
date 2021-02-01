2c08daebaf5a7829559a6fd4bde9e3a39d3e3a1c
Duotech Academy <duotechinstructor@gmail.com>
2021-02-01 18:57:11 -0500

    changed the tag

--- src/test/java/runners/CukeRunner.java
+++ src/test/java/runners/CukeRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @CucumberOptions (
 		
-		tags = "@temp",    //"@negative or @smoke", "@smoke and @negative and @login"
+		tags = "@myChange",    //"@negative or @smoke", "@smoke and @negative and @login"
 		
 		features = "src/test/resources/features",  // feature file folder path
 		glue = "step_definitions", // step def classes folder path
